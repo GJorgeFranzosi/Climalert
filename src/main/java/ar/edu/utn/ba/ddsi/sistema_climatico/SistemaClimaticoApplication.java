68da38e3d53270480792e575a4a4dae080515cd0
Guido Jorge Franzosi <gjorgefranzosi@frba.utn.edu.ar>
2026-07-04 15:16:16 -0300

    config spring boot

---
+++ src/main/java/ar/edu/utn/ba/ddsi/sistema_climatico/SistemaClimaticoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.ba.ddsi.sistema_climatico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaClimaticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaClimaticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/utn/ba/ddsi/sistema_climatico/SistemaClimaticoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.ba.ddsi.sistema_climatico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaClimaticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
