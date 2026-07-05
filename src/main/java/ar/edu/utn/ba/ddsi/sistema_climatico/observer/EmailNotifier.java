e27bb95e1dc3d497c7357f025dd5c7396e6125cf
Guido Jorge Franzosi <gjorgefranzosi@frba.utn.edu.ar>
2026-07-05 17:11:19 -0300

    Entrega final

--- src/main/java/ar/edu/utn/ba/ddsi/sistema_climatico/observer/EmailNotifier.java
+++ src/main/java/ar/edu/utn/ba/ddsi/sistema_climatico/observer/EmailNotifier.java
@@ -1,16 +1,32 @@
 package ar.edu.utn.ba.ddsi.sistema_climatico.observer;
 
 import ar.edu.utn.ba.ddsi.sistema_climatico.domain.Alerta;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class EmailNotifier implements ObservadorAlerta {
+  private final JavaMailSender mailSender;
 
   @Override
   public void actualizar(Alerta alerta) {
-    //TODO: dsp implemento con JavaMailSender
-    System.out.println("Enviando mail...");
-    System.out.println("Temperatura: " + alerta.getClima().getTemperatura());
-    System.out.println("Humedad: " + alerta.getClima().getHumedad());
+    SimpleMailMessage mensaje = new SimpleMailMessage();
+    mensaje.setTo(
+        "admin@clima.com",
+        "emergencias@clima.com",
+        "meteorologia@clima.com"
+    );
+    mensaje.setSubject("Alerta Climática");
+    mensaje.setText(
+        "Se detectó una alerta climática.\n\n" +
+            "Temperatura: " + alerta.getClima().getTemperatura() + " °C\n" +
+            "Humedad: " + alerta.getClima().getHumedad() + " %\n" +
+            "Descripción: " + alerta.getClima().getDescripcion() + "\n" +
+            "Fecha: " + alerta.getFecha()
+    );
+    mailSender.send(mensaje);
   }
 }
